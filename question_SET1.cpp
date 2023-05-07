92296c26b2bd80047ec32fdba2de75cb63adba37
Vansh <vanshayushi@gmail.com>
2023-05-07 18:35:39 +0530

    soln 13 done

--- question_SET1.cpp
+++ question_SET1.cpp
@@ -161,3 +161,180 @@
 //     cout<<"other: "<<other<<endl;
 //     return 0;
 // }
+
+
+// soln 8
+
+// #include<bits/stdc++.h>
+// using namespace std;
+ 
+// void print_array(int n,int arr[]){
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }   
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     } 
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n]; 
+//     print_array(n,arr);
+//     return 0;
+// }
+
+
+// soln 9
+
+// #include<bits/stdc++.h>
+// using namespace std;
+ 
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     bool check[1000]={false};
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }  
+//     for(int i=0;i<n;i++){                // {1,1,1,2,2}
+//         int cnt=0;                       // i= 0  j->0-4 
+//         for(int j=0;j<n;j++){
+//             if(arr[i]==arr[j]) cnt+=1;
+//         }        
+//         if(check[arr[i]]==false)cout<<"the element "<<arr[i]<<" is present "<<cnt<<" number of times\n";
+//         check[arr[i]]=true; 
+//     }
+//     return 0;
+// }
+
+// line 207 check[1]=false check[2]=flase
+// line 208 check[1]=true check[2]=true
+
+// soln 10
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void min_max(int n,int arr[]){
+//     int mn(1000000),mx(0);
+//     for(int i=0;i<n;i++){                // {4,3,5,2,1}
+//         if(arr[i] > mx){                 // i=0    mn=0  mx=5
+//             mx=arr[i];
+//         }
+//         if(arr[i] < mn){
+//             mn=arr[i];
+//         }
+//     }
+//     cout<<"the minimum value is "<<mn<<endl;
+//     cout<<"the maximum value is "<<mx;
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     min_max(n,arr);
+//     return 0;
+// }
+
+
+// soln 11
+
+// #include<bits/stdc++.h>
+// // #include <string>
+// using namespace std;
+ 
+// int main(){
+//     string s;
+//     cin>>s;            //  0 1 2 3 4
+//     int n=s.length();   // v a n s h
+//     for(int i=n-1 ; i>=0 ; i--){
+//         cout<<s[i];
+//     }
+//     return 0;
+// }
+
+
+// soln 12
+
+// #include<bits/stdc++.h>
+// using namespace std;
+ 
+// void rev(){
+//     string s;
+//     cin>>s;
+//     for(int i=s.length()-1 ; i>=0 ; i--){
+//         cout<<s[i];
+//     }
+// }
+
+// int main(){
+//     rev();   
+//     return 0;
+// }
+
+
+// soln 13
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void count_vowels(string str){
+//     int cnt=0;
+//     for(int i=0;i<str.length();i++){
+//         if(str[i]=='a' || str[i]=='A' || str[i]=='e' ||str[i]=='E' ||str[i]=='i' ||str[i]=='I' ||str[i]=='o' ||str[i]=='O' ||str[i]=='u' ||str[i]=='U'){
+//             cnt++;
+//         }
+//     }
+//     cout<<"number of vowels "<<cnt<<endl;
+//     cout<<"number of consonants "<<str.length()-cnt;
+// }
+
+// int main(){
+//     string s;
+//     cin>>s;
+//     count_vowels(s);
+//     return 0;
+// }
+
+// soln 14
+
+// RCB  vansh
+// BCR  ahnsv
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s;
+    cin>>s;
+    int n=s.length();
+    for(int i=0;i<n;i++){
+        for(int j=1;j<n;j++){
+            if(s[j-1]>s[j]){
+                char temp=s[j-1];
+                s.insert(j,to_string(temp));
+                s.insert(j-1,to_string(s[j]));
+            }
+        }
+    }
+    cout<<s<<endl;
+    return 0;
+}
+
+// #include<bits/stdc++.h>
+// using namespace std;
+ 
+// int main(){
+//     string s;
+//     cin>>s;
+//     sort(s.begin(),s.end());
+//     cout<<s;
+//     return 0;
+// }
