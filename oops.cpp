6b6665022ed9fe9397a695a21836f301f5d2679b
Vansh <vanshayushi@gmail.com>
2023-05-16 21:32:06 +0530

    oops

--- oops.cpp
+++ oops.cpp
@@ -1,37 +1,98 @@
 // classes and objects
 
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
  
-class cars{ 
-    private:  // priovate and public ->  Access Specifiers
-    int speed_limit;
-    string name;
-    string model;
-    string company;
-    public:
-    int speed(){
-        cout<<name;
-        cout<<speed_limit<<endl;
-    }
-    int speed(int n){
-        cout<<speed_limit*n<<endl;
-    }  
-    int speed(int n,int m){
-        cout<<speed_limit*n<<endl;
-        cout<<speed_limit*m<<endl;
-    }  
-};
+// class cars{ 
+//     private:  // priovate and public ->  Access Specifiers
+//     int speed_limit;
+//     string name;
+//     string model;
+//     string company;
+//     public:
+//     int speed(){
+//         cout<<name;
+//         cout<<speed_limit<<endl;
+//     }
+//     int speed(int n){
+//         cout<<speed_limit*n<<endl;
+//     }  
+//     int speed(int n,int m){
+//         cout<<speed_limit*n<<endl;
+//         cout<<speed_limit*m<<endl;
+//     }  
+// };
 
-int main(){
-    cars brezza; // sizeof(cars)
-    cout<<brezza.speed()<<endl;
-    cars x3;
-    return 0;
-}
+// int main(){
+//     cars brezza; // sizeof(cars)
+//     cout<<brezza.speed()<<endl;
+//     cars x3;
+//     return 0;
+// }
 
 
 // Encapsulation -> wrapping up data and information under a single unit.
 // Abstraction -> displaying only essential information and hiding the details.
 // Polymorphism -> ability of a message to be displayed in more than one form.
 // Inheritance -> capability of a class to derive properties and characteristics from another class.
+
+
+// classes and objects
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class cars{
+    private:
+    string name;
+    string model;
+    float milage;
+
+    public:
+    virtual void name_display(){
+        cout<<name<<" is the name of the car\n";
+    }
+    string show_name(){
+        return name;
+    }
+    void milage_change(int n){
+        milage=n;
+        cout<<"milage has been upadted to "<<n<<endl;
+    }
+    cars(string n,string m,int ml){
+        name=n;
+        model=m;
+        milage=ml;
+    }
+};
+
+class bikes:public cars{
+    private:
+    int length;
+
+    public:
+    bikes(string n,string m,int ml,int l) : cars(n,m,ml){
+        length=l;
+    }
+    void name_display(){
+        cout<<show_name()<<" is the name of the bike\n";
+    }
+};
+
+int main(){
+    cars c1=cars("brezza","lxi",15);
+    // c1.name_display();
+
+    cars c2=cars("bmw","X3",15);
+    // c2.name_display();
+    // c2.milage_change(10);
+
+    bikes b1=bikes("ktm","duke",20,2);
+    // b1.name_display();    
+
+    cars* car;
+    car=&b1;
+    car->name_display();
+    return 0;
+}
--- question_set2.cpp
+++ question_set2.cpp
@@ -4,7 +4,7 @@
 
 // 3)How to calculate the sum of digits using recursion in c++?
 
-// 4)How to solve the Tower of Hanoi problem using recursion? 
+// 4)How to solve the Tower of Hanoi problem using recursion?
 // To know more about the problem: https://www.educative.io/answers/what-is-the-tower-of-hanoi-problem
 
 // 5)Write a program to find all pairs of an integer array whose sum is equal to a given number?
@@ -15,20 +15,21 @@
 
 // soln 7
 
-//{1,2,3,4,5}  k=3  // {4,5,1,2,3}
+//{1,2,3,4,5}  k=3   
 
 // #include<bits/stdc++.h>
 // using namespace std;
- 
+
 // int main(){
 //     int n;
 //     int k;
 //     cin>>n>>k;
+//     k=k%n;
 //     int arr[n];
 //     for(int i=0;i<n;i++){
 //         cin>>arr[i];
 //     }
-//     for(int i=(n-k+1);i<n;i++){
+//     for(int i=(n-k+1);i<n;i++){  // i=3,4
 //         cout<<arr[i]<<" ";
 //     }
 //     for(int i=0;i<k;i++){
@@ -37,26 +38,25 @@
 //     return 0;
 // }
 
-
 // soln 6
 
 // {1,1,1,1,2,2,2,3,3}
 
 // #include<bits/stdc++.h>
 // using namespace std;
- 
+
 // int main(){
 //     int n;
 //     cin>>n;
 //     int arr[n];
 //     for(int i=0;i<n;i++){
 //         cin>>arr[i];
-//     }   
+//     }
 //     int ans=1;
 //     int mx=-100;
 //     for(int i=1;i<n;i++){              // {1,1,1,1,2,1,2,2,3,3}
-//         if(arr[i]!=arr[i-1]){          // i->9  ans=2
-//             mx=max(ans,mx);            // mx=max(2,4) => mx=4
+//         if(arr[i]!=arr[i-1]){          // i->5  ans=1
+//             mx=max(ans,mx);            // mx=max(4,2) => mx=4
 //             ans=1;
 //         }
 //         else ans++;
@@ -65,14 +65,13 @@
 //     return 0;
 // }
 
-
 // soln 5
 
 // {3,4,2,5,3,4} sum=6  (3,3) (2,4) (4,2) (2,4) (4,2)
 
 // #include<bits/stdc++.h>
 // using namespace std;
- 
+
 // int main(){
 //     int n;
 //     int sum;
@@ -91,7 +90,6 @@
 //     return 0;
 // }
 
-
 // soln 1
 
 // return (n)*factor(n-1)
@@ -100,7 +98,7 @@
 
 // #include<bits/stdc++.h>
 // using namespace std;
- 
+
 // int power(int n,int p){
 //     if(p==0) return 1;
 //     return n*power(n,p-1);
@@ -109,7 +107,7 @@
 // int main(){
 //     int n,p;
 //     cin>>n>>p;
-//     cout<<power(n,p);   
+//     cout<<power(n,p);
 //     return 0;
 // }
 
@@ -120,7 +118,6 @@
 // power(2,1) return 2*1=2;
 // power(2,0) return 1
 
-
 // soln 2
 
 // s="RCB" ... s=""
@@ -129,7 +126,7 @@
 
 // #include<bits/stdc++.h>
 // using namespace std;
- 
+
 // string rev(string s,int len){
 //     if(len==0) return "";
 //     return rev(s.substr(1,len-1),len-1)+s[0];
@@ -145,9 +142,8 @@
 // s="RCB" len=3
 // rev("RCB",3) return "BC"+'R'=>"BCR";
 // rev("CB",2) return "B"+'C'=>"BC";
-// rev("B",1) return ""+'B'=>"B"; 
+// rev("B",1) return ""+'B'=>"B";
 // rev("",0) return "";
-
 
 // soln 3
 
@@ -157,7 +153,7 @@
 
 // #include<bits/stdc++.h>
 // using namespace std;
- 
+
 // int digsum(int n){
 //     if(n==0) return 0;
 //     return n%10 + digsum(n/10);
@@ -176,8 +172,6 @@
 // digsum(1) return 1 + 0 = 1;
 // digsum(0) return 0;
 
-
-
 // soln 4
 
 // n=3 3 -> C rod
@@ -189,7 +183,7 @@
 
 // #include<bits/stdc++.h>
 // using namespace std;
- 
+
 // void tower(int n,char frm,char to ,char help){
 //     if(n==0) return;
 //     tower(n-1,frm,help,to);
@@ -206,6 +200,33 @@
 
 // n=3
 
-// tower(3,A,C,B) tower(2,A,B,C) => "move the disc 2 from A to B"  => 2nd  
+// tower(3,A,C,B) tower(2,A,B,C) => "move the disc 2 from A to B"  => 2nd
 //                tower(1,A,C,B) => "move the disc 1 from A to C"  => 1st   tower(1,C,B,A) => "move the disc 1 from C to B"
-//                tower(0,A,B,C) return 
+//                tower(0,A,B,C) return
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int soln(int a[], int n)
+// {
+//     if (n >= 0)
+//         return a[n-1] + soln(a, n - 1);
+//     else
+//         return 0;
+// }
+// int main()
+// {
+//     int n;
+//     cout << "Enter number of elements" << endl;
+//     cin >> n;
+//     int a[n];
+//     cout << "Enter the elemnts" << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> a[i];
+//     }
+//     cout << soln(a, n);
+//     return 0;
+// }
+
+
+// 456 => 4 + 5 + 6 => 15
